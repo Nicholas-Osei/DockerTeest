8bd64fba8b11c4ff4c55aefee1dc7f49b7021392
Nicholas Osei <Niconaldodonjunior@yahoo.com>
2021-09-25 11:21:50 +0200

    test 1

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+// app.get('/', (req,res)=> {
+//   res.sendFile(__dirname + '/client/index.html');
+// })
+
+// app.listen(8080, () => console.log('Server ready'))
+// const express = require('express')
+// const app = express()
+
+app.get('/', (req,res)=> {
+  res.sendFile(__dirname + '/client/index.html');
+})
+
+//app.listen(8080, () => console.log('Server ready'))
+app.listen(process.env.PORT || 3000, () => console.log('server ready'))
