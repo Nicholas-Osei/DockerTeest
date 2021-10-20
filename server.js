c73c54e9d565c2abdeaa9dfd89e8c4a7562f8198
Nicholas Osei <Niconaldodonjunior@yahoo.com>
2021-10-20 09:12:52 +0200

    jk

--- server.js
+++ server.js
@@ -9,12 +9,9 @@ const app = express()
 // const express = require('express')
 // const app = express()
 
-// app.get('/', (req,res)=> {
-//   res.sendFile(__dirname + '/client/index.html');
-// })
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
+app.get('/', (req,res)=> {
+  res.sendFile(__dirname + '/client/index.html');
+})
 
 //app.listen(8080, () => console.log('Server ready'))
-app.listen(process.env.PORT || 8080, () => console.log('server ready'),console.log(process.env.PORT))
+app.listen(process.env.PORT || 3000, () => console.log('server ready'),console.log(process.env.PORT))
