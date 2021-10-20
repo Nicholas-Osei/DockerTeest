2e2f70e48952bb0145cd960d772934a0a33e0859
Nicholas Osei <Niconaldodonjunior@yahoo.com>
2021-10-20 09:41:49 +0200

    tetst

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
+
+// // app.get('/', (req,res)=> {
+// //   res.sendFile(__dirname + '/client/index.html');
+// // })
+
+// // app.listen(8080, () => console.log('Server ready'))
+// // const express = require('express')
+// // const app = express()
 
 // app.get('/', (req,res)=> {
 //   res.sendFile(__dirname + '/client/index.html');
 // })
 
 // app.listen(8080, () => console.log('Server ready'))
-// const express = require('express')
-// const app = express()
+// //app.listen(process.env.PORT || 3000, () => console.log('server ready'),console.log(process.env.PORT))
 
-app.get('/', (req,res)=> {
-  res.sendFile(__dirname + '/client/index.html');
-})
+const config = require('./config');
 
-//app.listen(8080, () => console.log('Server ready'))
-app.listen(process.env.PORT || 3000, () => console.log('server ready'),console.log(process.env.PORT))
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Azure!!!');
+});
+
+app.listen(config.port, () => {
+  console.log(`Server started on port ${config.port}`);
+});
